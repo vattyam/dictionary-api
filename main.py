a9a00bd7cca5a68ccf14cd7476d87f6f3601a70d
sandeep <vattyam.sandeep@gmail.com>
2024-12-16 12:32:08 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/api/v1/<word>")
+def about(word):
+    word_local = word.upper()
+    result = {"definition": word_local,
+            "word": word}
+    return result
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
