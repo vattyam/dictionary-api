2b872ceeee939d10071e6ebf2579b961b67f3127
sandeep <vattyam.sandeep@gmail.com>
2024-12-16 16:09:08 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
+df = pd.read_csv("dictionary.csv")
 
 @app.route("/")
 def home():
@@ -9,8 +11,8 @@ def home():
 
 @app.route("/api/v1/<word>")
 def about(word):
-    word_local = word.upper()
-    result = {"definition": word_local,
+    definition_local = df.loc[df['word'] == word]['definition'].squeeze()
+    result = {"definition": definition_local,
             "word": word}
     return result
 
